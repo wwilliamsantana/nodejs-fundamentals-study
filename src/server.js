736b0eb1471e531fed4c916097b36c35524af5bc
William Santana <euwilliamsantana@gmail.com>
2025-04-01 07:17:20 -0300

    feat: middleware buffer

--- src/server.js
+++ src/server.js
@@ -1,33 +1,22 @@
 import http from 'node:http';
+import { randomUUID } from 'node:crypto'
+import { json } from './middlewares/json.js';
 
 const users = []
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
-
+  await json(req, res)
 
   if (method === "GET" && url === "/users") {
-    return res
-      .setHeader("Content-Type", "application/json")
-      .end(JSON.stringify(users))
+    return res.end(JSON.stringify(users))
   }
 
   if (method === "POST" && url === "/users") {
     const { name, email } = req.body
     users.push({
-      id: 1,
+      id: randomUUID(),
       name,
       email
     })
